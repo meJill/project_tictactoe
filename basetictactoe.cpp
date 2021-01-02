e764ed953d3c44ce010fec84a9f1757ca595e608
meJill <76845038+meJill@users.noreply.github.com>
2021-01-02 16:09:37 +0800

    Update basetictactoe.cpp

--- basetictactoe.cpp
+++ basetictactoe.cpp
@@ -1,250 +1,137 @@
 #include <stdio.h>
 #include <conio.h>
 
+//NEW BASE 5 x 5  TICTACTOE == 2 Players;
+char display[5][5];
 
 
-char a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i', j = 'j', k = 'k', l = 'l' , m = 'm',
-	n = 'n', o = 'o', p = 'p', q = 'q', r = 'r', s = 's', t = 't', u = 'u', v = 'v', w = 'w', x = 'x', y = 'y';
-
-	
-	
-void board()
-{	
-
-	printf("---------------------\n");
-	printf("| %c | %c | %c | %c | %c |\n", a, b ,c, d, e);
-	
-	printf("---------------------\n");
-	
-	printf("| %c | %c | %c | %c | %c |\n", f, g, h, i, j);
-	
-	printf("---------------------\n");
-	
-	printf("| %c | %c | %c | %c | %c |\n", k, l, m, n, o);
-	
-	printf("---------------------\n");
-	
-	printf("| %c | %c | %c | %c | %c |\n", p, q, r, s, t);
-	
-	printf("---------------------\n");
-
-	printf("| %c | %c | %c | %c | %c |\n", u, v, w, x, y );
-	
-	printf("---------------------\n");
-
-}
-
-
-int Win()
+void board() //diplay board
 {
+	int i;
 
-	
-	if(a == b && b == c && c == d && d == e)
-	{
-		return 1;
+	for(i=0;i<5;i++){
+		printf("       |     |     |     |\n");
+		printf("    %c  |  %c  |  %c  |  %c  |  %c  \n",display[i][0],display[i][1],display[i][2],display[i][3],display[i][4]);
+		if(i!=4)
+			printf("  ----- ----- ----- ----- ----- \n");
 	}
-	else if(f == g && g == h && h == i && i == j)
-	{
-		return 1;
-	}
-	else if(k == l && l == m && m == n && n == o)
-	{
-		return 1;
-	}
-	else if(p == q && q == r && r == s && s == t)
-	{
-		return 1;
-	}
-	else if(u == v && v == w && w == x && x == y)
-	{
-		return 1; 
-	} 
-	else if(a == f && f == k && k == p && p == u)
-	{
-	return 1; 
-	} 
-	else if(b == g && g == l && l == q && q == v)
-	{
-	return 1; 
-	} 
-	else if(c == h && h == m && m == r && r == w)
-	{
-	return 1; 
-	}
-	else if(d == i && i == n && n == s && s == x)
-	{
-	return 1; 
-	} 
-	else if(e == j && j == o && o == t && t == y)
-	{
-	return 1; 
-	} 
-	else if(a == g && g == m && m == s && s == y)
-	{
-	return 1; 
-	}
-	else if(e == i && i == m && m == q && q == u)
-	{
-	return 1; 
-	}  
-	else if(a != 'a'&& b != 'b'&& c != 'c'&& d != 'd'&& e != 'e'&& f != 'f'&& g != 'g'&& h != 'h'&& i != 'i'&& j != 'j'&& k != 'k'&& l != 'l' && m != 'm'&&
-	n != 'n'&& o != 'o'&& p != 'p'&& q != 'q'&& r != 'r'&& s != 's'&& t != 't'&& u != 'u'&& v != 'v'&& w != 'w'&& x != 'x'&& y != 'y')
-	{
-		return 0;
-	}
-	else
-	{
-		return - 1; 
-	}
-	 
+	printf("\n\n");
 }
 
-int normal(char choice, int player, char sign)
-{	
+void displayboard(int n)			//initialize board														
+{
+	int i,j;
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			display[i][j]=' ';														//initially all indices are made empty
 
-			}
+}
 
+void player1()
+{
+	int x,y;
+
+	printf("Enter x and y co-ordinates of your move player 1: ");
+	scanf("%d %d",&x,&y);															//user to input the x & y co-ordinate of his move
+
+	if(display[--x][--y]!=' '){														//if his input index is not empty,invalid move is shown
+		printf("Invalid move,try again\n\n");
+		player1();
+	}
+	else 
+	{
+		display[x][y]='O';	
+	}
+							
+	printf("\n\n\n");
+}
+
+void player2()
+{
+	int x,y;
+
+	printf("Enter x and y co-ordinates of your move player 2: ");
+	scanf("%d %d",&x,&y);															//user to input the x & y co-ordinate of his move
+
+	if(display[--x][--y]!=' '){														//if his input index is not empty,invalid move is shown
+		printf("Invalid move,try again\n\n");
+		player2();
+	}
+	else 
+	{
+		display[x][y]='X';	
+	}
+							
+	printf("\n\n\n");
+}
+
+char Win()
+{
+	int i,j;
+
+
+	for(i=0;i<4;i++)
+		if((display[i][0]==display[i][1]&&display[i][1]==display[i][2])&&(display[i][2]==display[i][3]&&display[i][3]==display[i][4]))							
+			return display[i][0];
+
+
+	for(i=0;i<4;i++)
+		if((display[0][i]==display[1][i]&&display[1][i]==display[2][i])&&(display[2][i]==display[3][i]&&display[3][i]==display[4][i]))							
+			return display[0][i];
+
+
+	if((display[0][0]==display[1][1]&&display[1][1]==display[2][2])&&(display[2][2]==display[3][3]&&display[3][3]==display[4][4]))								
+		return display[0][0];
+
+
+	if((display[0][4]==display[1][3]&&display[1][3]==display[2][2])&&(display[2][2]==display[3][1]&&display[3][1]==display[4][0]))								
+			return display[0][4];
+
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if(display[i][j]==' ')
+				return ' ';																	//if we find any empty index,we simply return it
+		}
+	}
+
+	//there is no identical rows,columns or diagonals,there is no empty indices left means the match is drawn and we return 'D'
+
+	return 'D';
+}
 
 int main()
 {
-	int player = 1, z;
-	int yn;
-	char choice, sign, box_choice;
+	char fin =' ';
 	
-	do{ 
+	displayboard(5);
 	
-		board();
-		player = (player % 2);
-		if(player != 0)
-		{
-			player = 1;
-		}
-		else
-		{
-			player = 2;
-		}
-		
-			if(player == 1)
-		{
-			sign = 'O';
-		}
-		else
-		{
-			sign = 'X';
-		}
-		
-		printf("Player %d, enter your move : \n", player);
-		scanf("%c", &choice);
-		
-		if(choice == 'a' & a == 'a') 
-			a = sign;
+	do{
+			player1();																//player's move
+			board();															//displaying the current matrix
+			fin=Win();	
 			
-		else if(choice == 'b' && b == 'b')
-			b = sign;
+			if(fin!=' ')
+				break;	
+				
+			player2();																//player's move
+			board();															//displaying the current matrix
+			fin=Win();	
 			
-		else if(choice == 'c' && c == 'c')
-			c = sign;
-			
-		else if(choice == 'd' && d == 'd')
-			d = sign;
-			
-		else if(choice == 'e' && e == 'e')
-			e = sign;
-			
-		else if(choice == 'f' && f == 'f')
-			f = sign;
-			
-		else if(choice == 'g' && g == 'g')
-			g = sign;	
-					
-		else if(choice == 'h' && h == 'h')
-			h = sign;
-			
-		else if(choice == 'i' && i == 'i')
-			i = sign;	
-			
-		else if(choice == 'j' && j == 'j')
-			j = sign;
-			
-		else if(choice == 'k' && k == 'k')
-			k = sign;
-			
-		else if(choice == 'l' && l == 'l')
-			l = sign;
-			
-		else if(choice == 'm' && m == 'm')
-			m = sign;	
-			
-		else if(choice == 'n' && n == 'n')
-			n = sign;
-			
-		else if(choice == 'o' && o == 'o')
-			o = sign;	
-			
-		else if(choice == 'p' && p == 'p')
-			p = sign;
-			
-		else if(choice == 'q' && q == 'q')
-			q = sign;	
-			
-		else if(choice == 'r' && r == 'r')
-			r = sign;
-			
-		else if(choice == 's' && s == 's')
-			s = sign;	
-			
-		else if(choice == 't' && t == 't')
-			t = sign;
-			
-		else if(choice == 'u' && u == 'u')
-			u = sign;	
-			
-		else if(choice == 'v' && v == 'v')
-			v = sign;
-			
-		else if(choice == 'w' && w == 'w')
-			w = sign;	
-			
-		else if(choice == 'x' && x == 'x')
-			x = sign;	
-						
-		else if(choice == 'y' && y == 'y')
-			y = sign;		
-																														
-		else
-			player--;
-			printf("Input incorrect \n");
-			getchar();
+	}while(fin ==' ');
+	
+	if(fin=='X')																	//if the return is 'X',user won
+		printf("Player 2 won\n");														
+	else if(fin=='O')																//if the return is 'O',computer won
+		printf("Player 1 won\n");													
+	else if(fin=='D')																//if the return is 'D',match drawn
+		printf("Match Drawn\n");
 
+	return 0;
 		
-		
-		z = Win();
-		
-		player++;
-		
-		}while(z == - 1 );
-
-		board();
-		
-		if (z == 1)
-	        printf("==>\aPlayer %d win ", --player);
-	    else
-	        printf("==>\aGame draw");
-	
-	    getch();
-	
-	    return 0;
-	}
-
-
-	
+}
 		
 
 	
-
-
-
-
 
 
 
